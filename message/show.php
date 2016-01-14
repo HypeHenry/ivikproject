ac2a40cb233cb30361b930afcd106bd553d2519f
Henry Barima <H.Barima@me.com>
2016-01-14 11:24:38 +0100

    Lijst maken met behulp van een Session key.

--- message/show.php
+++ message/show.php
@@ -4,3 +4,21 @@
 this file will show list of messages 
  */
 
+include 'header.php';
+session_start();
+
+$id = $_GET['id'];
+
+foreach ($_SESSION['messages'] as $key => $message){
+    
+    if($key == $id){
+                        
+            echo "<div id=\"message\" class=\"list-group\">";
+            echo "<a class=\"list-group-item\"><h4> Title: ". $message['title'] . "</h4></a>";
+            echo "<textarea name=\"content\" type=\"text\" class=\"form-control\" id=\"content\">". $message['content'] . "</textarea>";
+            echo "<br>";
+            echo "</div>";
+            }
+             
+}
+include 'footer.php';
